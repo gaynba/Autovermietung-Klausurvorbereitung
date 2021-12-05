0b25b8773a2ee07afd6ca643800e7d3a9bb7ff7d
gaynba <11-lenny@web.de>
2021-12-05 15:27:30 +0100

    init

--- Autovermietung-Klausurvorbereitung/src/view/Output.java
+++ Autovermietung-Klausurvorbereitung/src/view/Output.java
@@ -7,20 +7,5 @@ public class Output {
         System.out.println(data);
     }
 
-    public static void addObject(Object ele, Object[] array){
-        int i = 0;
-        for(Object a : array){
-            if(a == null){
-                array[i] = ele;
-                break;
-            }
-            i++;
-        }
-
-    }
-
-
-    public void showSport(){
-        System.out.println();
-    }
+    
 }
